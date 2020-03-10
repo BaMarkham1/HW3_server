a6e673c844edc549564a44ef4e6cd54ed87e2c04
BaMarkham1 <benjamin.lemarc@ucdenver.edu>
2020-03-10 17:33:55 -0600

    first commit

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ router.route('/movies')
 
         Movie.findOne({ title: movieNew.title }).select('*').exec(function(err, movie) {
             if (err) res.send(err);
-            res.status(200).send({msg : "GET movies", movie: movie});
+            res.status(200).send({msg : "GET movies", title: movie.title, genre: movie.genre});
         });
     });
 
